6ad4047cec7107b76bd2dcb263f6744b13685518
Phil <PhilboBaggins@users.noreply.github.com>
2019-06-07 03:51:50 +1000

    Add basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("CryptXor")
+        .version("1.0")
+        .about("????? TODO: Add description")
+        .author("Phil B.")
+        .arg(Arg::with_name("input-file")
+            .help("Input file")
+            .takes_value(true)
+            .required(true)
+            .multiple(false))
+        .arg(Arg::with_name("output-file")
+            .help("Output file")
+            .takes_value(true)
+            .required(true)
+            .multiple(false))
+        .get_matches();
+
+    let (input_file, output_file) =
+        (matches.value_of("input-file").unwrap(),
+         matches.value_of("output-file").unwrap());
+
+    println!("Input file  = {}", input_file);
+    println!("Output file = {}", output_file);
 }
